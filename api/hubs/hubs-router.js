2ae6be61703693dd5923344f9470f79dd3bcb7f5
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-05 20:16:18 +0200

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -5,15 +5,6 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
-function logger(req, res, next) {
-  console.log(`
-    ${req.method} request to ${req.baseUrl} endpoint!
-    req.body ${JSON.stringify(req.body)}
-    req.params.id ${req.params.id}
-  `)
-  next()
-}
-
 router.get('/', logger, (req, res, next) => {
   Hubs.find(req.query)
     .then(hubs => {
@@ -21,20 +12,6 @@ router.get('/', logger, (req, res, next) => {
     })
     .catch(next);
 });
-
-async function idChecker(req, res, next) {
-  try {
-    const hub = await Hubs.findById(req.params.id)
-    if (!hub) {
-      next({ status: 404, message: `hub with id ${req.params.id} not found!` })
-    } else {
-      req.hub = hub
-      next()
-    }
-  } catch (err) {
-    next(err)
-  }
-}
 
 router.get('/:id', logger, idChecker, (req, res) => {
   res.json(req.hub)
@@ -71,32 +48,6 @@ router.get('/:id/messages', logger, idChecker, (req, res, next) => {
     })
     .catch(next);
 });
-
-const messageSchema = yup.object({
-  sender: yup.string()
-    .trim()
-    .required('sender required')
-    .min(3, 'sender must be 3 chars')
-    .max(40, 'sender must be under 40 chars'),
-  text: yup.string()
-    .trim()
-    .required('text required')
-    .min(3, 'text must be 3 chars')
-    .max(40, 'text must be under 40 chars'),
-})
-
-async function validateMessage(req, res, next) {
-  try {
-    const validated = await messageSchema.validate(req.body, {
-      stripUnknown: true,
-    })
-    req.body = validated
-    next()
-  } catch (err) {
-    // here validation failed
-    next({ status: 400, message: err.message })
-  }
-}
 
 router.post(
   '/:id/messages',
