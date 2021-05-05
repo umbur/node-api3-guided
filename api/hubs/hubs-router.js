d5b58fe1b4426ca8c258feeb952d522038f7b770
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-05 18:59:36 +0200

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -64,7 +64,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', logger, idChecker, (req, res) => {
   Hubs.remove(req.params.id)
     .then(count => {
       if (count > 0) {
