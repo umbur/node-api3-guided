bae3c38bf320e41fc0295795bfc160d473ec7cb0
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-05 18:40:07 +0200

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -12,7 +12,7 @@ function logger(req, res, next) {
   next()
 }
 
-router.get('/', logger, (req, res, next) => {
+router.get('/', (req, res, next) => {
   Hubs.find(req.query)
     .then(hubs => {
       res.status(200).json(hubs);
@@ -25,6 +25,8 @@ router.get('/', logger, (req, res, next) => {
       });
     });
 });
+
+router.use(logger)
 
 router.get('/:id', (req, res) => {
   Hubs.findById(req.params.id)
